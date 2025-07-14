f7dba2862753d8959d7b606a651ebd9fa7d86b36
luser <3087151659@qq.com>
2025-07-14 15:33:23 +0800

    这是下午三点三十三的测试

---
+++ luser_test/module2.py
@@ -0,0 +1,5 @@
+def plus(a : int , b : int):
+    return a * b
+
+if __name__ == '__main__':
+    print(plus(2, 3))
---
+++ test02.py
@@ -0,0 +1,5 @@
+import luser_test.module1
+import luser_test.module2
+
+print(luser_test.module1.add(1, 2))
+print(luser_test.module2.plus(2, 3))
